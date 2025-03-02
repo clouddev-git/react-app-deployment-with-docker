e08ec4a7e9b0ab1cd6868cc5239e49b594bec7fb
clouddev-git <root@ip-172-31-12-240.ap-south-1.compute.internal>
2025-03-02 19:38:21 +0000

    arul coomited

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 docker login -u arul143 -p Vijayalakshmi@2718
 docker stop reactcon
 docker rm reactcon
-docker build -t reactapplication .
-docker run -itd --name react -p 80:80 reactapplication
-docker tag reactapplication arul143/reactapplication:ci-cd
-docker push arul143/reactapplication:ci-cd
+docker build -t finalimg .
+docker run -itd --name react -p 80:80 finalimg
+docker tag finalimg arul143/finalapp:ci-cd
+docker push arul143/finalapp:ci-cd

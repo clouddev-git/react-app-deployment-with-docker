0d225fc1e9ba8c2d63f172963c29ecdd15f8b11f
clouddev-git <root@ip-172-31-1-12.ap-south-1.compute.internal>
2025-03-24 08:52:59 +0000

    arul changed

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 docker login -u arul143 -p Vijayalakshmi@2718
-docker stop procon
-docker rm procon
 docker build -t reactimg .
 docker run -itd --name reactcon -p 80:80 reactimg
 docker tag reactimg arul143/reactapplication:ci-cd

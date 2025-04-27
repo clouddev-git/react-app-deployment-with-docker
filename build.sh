f07915c0eac659982259b0ace32d6ce173636758
clouddev-git <root@ip-172-31-6-246.ap-south-1.compute.internal>
2025-04-27 03:22:38 +0000

    recently updated

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 docker login -u arul143 -p Vijayalakshmi@2718
-docker stop mycon 
-docker rm -f mycon
-docker build -t reactimg .
-docker run -itd --name reactcon -p 80:80 reactimg
-docker tag reactimg arul143/reactapplication:ci-cd
-docker push arul143/reactapplication:ci-cd
+docker build -t proimg .
+docker run -itd --name procon -p 80:80 proimg
+docker tag proimg arul143/proapplication:ci-cd
+docker push arul143/proapplication:ci-cd
